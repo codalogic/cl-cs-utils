5ef741745f05338cb96c2a46d32a3e00a3df0fb4
Pete Cordell <pete.git@codalogic.com>
2020-05-06 19:09:54 +0100

    Add ChunkedUTF8ToSubString functionality

--- cl-cs-utils/ChunkedUTF8ToSubString.cs
+++ cl-cs-utils/ChunkedUTF8ToSubString.cs
@@ -8,12 +8,18 @@ namespace cl_cs_utils
 {
     public class ChunkedUTF8ToSubString
     {
+        Decoder utf8Decoder = Encoding.UTF8.GetDecoder();   // This allows partial UTF-8 sequences in a block. See https://docs.microsoft.com/en-us/dotnet/api/system.text.utf8encoding.getdecoder?view=netcore-3.1
+        char[] chars = null;
+
         public ChunkedUTF8ToSubString()
         {}
 
         public string Transform( byte[] utf8, int length )
         {
-            return "";
+            if( chars == null || chars.Length < length )
+                chars = new char[length];
+            int charsDecodedCount = utf8Decoder.GetChars( utf8, 0, length, chars, 0);
+            return new string( chars, 0, charsDecodedCount );
         }
     }
 }
--- cl-cs-utilsTests/ChunkedUTF8ToSubStringTests.cs
+++ cl-cs-utilsTests/ChunkedUTF8ToSubStringTests.cs
@@ -12,10 +12,56 @@ namespace cl_cs_utils.Tests
     public class ChunkedUTF8ToSubStringTests
     {
         [TestMethod()]
+        public void TransformEmptyStringTest()
+        {
+            var cbtss = new ChunkedUTF8ToSubString();
+            Assert.IsTrue( cbtss.Transform( new byte[] {}, 0 ) == "" );
+        }
+
+        [TestMethod()]
         public void TransformSimpleASCIIStringTest()
         {
             var cbtss = new ChunkedUTF8ToSubString();
-            Assert.Fail();
+            Assert.IsTrue( cbtss.Transform( new byte[] { (byte)'T', (byte)'e', (byte)'s', (byte)'t' }, 4 ) == "Test" );
+        }
+
+        [TestMethod()]
+        public void TransformDualSimpleASCIIStringTest()
+        {
+            var cbtss = new ChunkedUTF8ToSubString();
+            var s1 = cbtss.Transform( new byte[] { (byte)'M', (byte)'o', (byte)'r', (byte)'e' }, 4 );
+            var s2 = cbtss.Transform( new byte[] { (byte)'T', (byte)'e', (byte)'s', (byte)'t' }, 4 );
+            Assert.IsTrue( s1 + s2 == "MoreTest" );
+        }
+
+        // UTF-8 C2 A3 is Pound Sterling code point (Unicode U+00A3)
+        [TestMethod()]
+        public void TransformNonASCIIStringTest()
+        {
+            var cbtss = new ChunkedUTF8ToSubString();
+            var s1 = cbtss.Transform( new byte[] { 0xC2, 0xA3 }, 2 );
+            Assert.IsTrue( s1 == "\u00A3" );
+        }
+
+        [TestMethod()]
+        public void TransformSplitNonASCIIStringTest()
+        {
+            var cbtss = new ChunkedUTF8ToSubString();
+            var s1 = cbtss.Transform( new byte[] { 0xC2 }, 1 );    // Pound Sterling code point
+            Assert.IsTrue( s1 == "" );
+            var s2 = cbtss.Transform( new byte[] { 0xA3 }, 1 );
+            Assert.IsTrue( s2 == "\u00A3" );
+        }
+
+        [TestMethod()]
+        public void TransformMidSplitNonASCIIStringTest()
+        {
+            var cbtss = new ChunkedUTF8ToSubString();
+            var s1 = cbtss.Transform( new byte[] { (byte)'-', 0xC2 }, 2 );
+            Assert.IsTrue( s1 == "-" );
+            var s2 = cbtss.Transform( new byte[] { 0xA3, (byte)'-' }, 2 );
+            Assert.IsTrue( s2 == "\u00A3-" );
+            Assert.IsTrue( s1 + s2 == "-\u00A3-" );
         }
     }
 }
