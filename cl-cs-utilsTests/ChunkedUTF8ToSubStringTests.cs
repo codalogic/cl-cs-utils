75e645edd48fabdb1caa174640c79bb0ecc090f2
Pete Cordell <pete.git@codalogic.com>
2020-05-06 22:12:54 +0100

    Removing unnessary "using" statements from ChunkedUTF8ToSubString

--- cl-cs-utilsTests/ChunkedUTF8ToSubStringTests.cs
+++ cl-cs-utilsTests/ChunkedUTF8ToSubStringTests.cs
@@ -1,10 +1,4 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using cl_cs_utils;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace cl_cs_utils.Tests
 {
