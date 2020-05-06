8c02d0434a76f96991f8ada1bac64b2717d19e2a
Pete Cordell <pete.git@codalogic.com>
2020-05-06 22:13:38 +0100

    Add description comment at start of ChunkedUTF8ToSubString

--- cl-cs-utils/ChunkedUTF8ToSubString.cs
+++ cl-cs-utils/ChunkedUTF8ToSubString.cs
@@ -1,5 +1,26 @@
 ﻿using System.Text;
 
+/*
+ * Allow blocks of bytes representing UTF-8 characters to be decoded into strings.
+ *
+ * An issue arises when converting blocks of UTF-8 encoded bytes into strings that
+ * a break in a read block of data may be mid way through a UTF-8 multi-byte sequence.
+ * This class Encoding.UTF8.GetDecoder() to store any trailing partial UTF-8 sequence
+ * and then save those bytes for the next call to the Tranform() function.
+ *
+ * Use as:
+        var utf8ToString = new ChunkedUTF8ToSubString();
+        var utf8 = new byte[10];
+        var str = "";
+        int count;
+
+        while( (count = stream.Read( utf8, 0, 2 )) != 0 )
+        {
+            str += utf8ToString.Transform( utf8, count );
+        }
+ *
+ */
+
 namespace cl_cs_utils
 {
     public class ChunkedUTF8ToSubString
