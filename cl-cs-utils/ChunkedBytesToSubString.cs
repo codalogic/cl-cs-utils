e19d1a74ef48f29a14caf229e1cffe75108a3bf6
Pete Cordell <pete.git@codalogic.com>
2020-05-06 18:15:19 +0100

    Add initial project file structure

---
+++ cl-cs-utils/ChunkedBytesToSubString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cl_cs_utils2
+{
+    public class ChunkedBytesToSubString
+    {
+        public ChunkedBytesToSubString()
+        {}
+
+        public string Transform( byte[] bytes, int length )
+        {
+            return "";
+        }
+    }
+}
---
+++ cl-cs-utilsTests/ChunkedBytesToSubStringTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using cl_cs_utils2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cl_cs_utils2.Tests
+{
+    [TestClass()]
+    public class ChunkedBytesToSubStringTests
+    {
+        [TestMethod()]
+        public void TransformTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
